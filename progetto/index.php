256eb9410dc2b936b0beb6d779a56ea59d51a6cb
amm <amm@amm-VirtualBox>
2015-08-24 18:03:47 +0200

    Prima versione

--- progetto/index.php
+++ progetto/index.php
@@ -3,15 +3,27 @@
 To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
+http://localhost/amm2015/progetto/index.php
 -->
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Cinema Pasolini - Home</title>
+        <link rel="stylesheet" type="text/css" href="css_ind.css">
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <h1>Cinema Pasolini</h1>
+        <nav>
+            <ul>
+                <li><b>In sala</b></li>
+                <br/>
+                <li><b>Prenota online</b></li>
+            </ul>
+        </nav>
+        <img title="cinema" alt="Foto del cinema" src="Cinema.jpg" height="500" width="800">
+        <article>
+            <h2>News!</h2>
+            <p>Il cinema rimarrà chiuso per ferie dal 12/08 al 20/08.</p>
+        </article>
     </body>
 </html>
