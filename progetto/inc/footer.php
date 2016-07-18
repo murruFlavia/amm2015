89d9aa2e43c112633b0793abef9c0463a23a3fbd
amm <amm@amm-VirtualBox>
2016-07-18 16:46:43 +0200

    modifiche minori

--- progetto/inc/footer.php
+++ progetto/inc/footer.php
@@ -1,9 +1,4 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
+
 
 <div id="footer">
     <br> Cinema Pasolini - Cagliari, via Michele Salvemini 33 - Mail: cinemapasolini@gmail.com <br/>
--- progetto/inc/header.php
+++ progetto/inc/header.php
@@ -1,9 +1,4 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
+
 
 <a href="../services/home.php"><h1>Cinema Pasolini</h1></a>
 <div id="mini_layer"></div>
